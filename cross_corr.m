f7a496ed6e7c522ac624b491270f9a447f98185e
giovanni-bort <63361927+giovanni-bort@users.noreply.github.com>
2020-04-11 14:54:15 +0200

    Add files via upload

---
+++ cross_corr.m
@@ -0,0 +1,39 @@
+% Cross correlation
+function[FP_,matrix]=cross_corr(lead,FP,Hz,Left,Right);
+if FP(1)==FP(2); FP=FP(2:length(FP)); end
+On=FP-round(Left*Hz/1000);    %80 ms left
+Off=FP+round(Right*Hz/1000);  %100 ms right
+if On(1)>0; k=1; else k=2; end
+if Off(length(Off))>length(lead); l=2; else l=1; end
+
+for i=k:length(FP)-l;
+    s1=lead(On(i):Off(i));
+    s2=lead(On(i+1):Off(i+1));
+    X1=xcorr(s1,s2); %compute cross-correlation between vectors s1 and s2
+    [m,FP_(i)]=max(X1);
+    FP_(i)=FP(i)+FP_(i)-length(s1); %shift index FP_, as length(X1)=2*N-1; where N is the length of the signals
+end
+if k==1; FP_=[FP(1) FP_]; %to equalise the length
+else FP_=[FP(1) FP_(2:length(FP_))];
+end
+
+% Matrix concatenation
+Size=size(lead);        % raw or coloumn ?
+if Size(1)~=1; lead=lead'; end
+if FP_(1)>FP_(2); m=FP_(1); FP_(1)=FP_(2); FP_(2)=m;  end
+
+% Boundery effects
+m=2;
+if FP_(1)-round(Left*Hz/1000)>1; matrix=[lead(FP_(1)-round(Left*Hz/1000) : FP_(1)+round(Right*Hz/1000))];
+else
+    if FP_(2)-round(Left*Hz/1000)>1; matrix=lead(FP_(2)-round(Left*Hz/1000) : FP_(2)+round(Right*Hz/1000));
+    else matrix=lead(FP_(3)-round(Left*Hz/1000) : FP_(3)+round(Right*Hz/1000)); m=3;
+    end
+end
+
+if FP_(length(FP_))+Right>length(lead); FP_=FP_(1:length(FP_)-1); end
+if(FP_(1)-round(Left*Hz/1000)<1),fprintf('***%6.0f%6.0f\n',FP_(1),round(Left*Hz/1000));end %********** MODIFY *******
+for i=m:length(FP_);
+    new=lead(FP_(i)-round(Left*Hz/1000) : FP_(i)+round(Right*Hz/1000));
+   matrix=[matrix; new];
+end
---
+++ load_12ECG_model.m
@@ -0,0 +1,10 @@
+function model = load_12ECG_model()
+model=0;
+    
+%         filename='finalized_model.mat';
+%         A=load(filename);
+%         model=A.model;
+
+end
+
+
